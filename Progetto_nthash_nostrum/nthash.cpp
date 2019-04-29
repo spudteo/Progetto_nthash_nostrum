c37181ea9c6b69532c4da834a79e62c1f6079234
spudteo <37929393+spudteo@users.noreply.github.com>
2019-04-29 13:53:27 +0200

    manca metodo per fare parsing e per calcolare gli hash con il metodo nostro

--- Progetto_nthash_nostrum/nthash.cpp
+++ Progetto_nthash_nostrum/nthash.cpp
@@ -32,8 +32,9 @@ uint64_t metodoNuovo_restantiHash(string past, string token, vector<int> ab, uin
 uint64_t hash_stupido(string sequenza_input, string spacedSeed);
 vector<uint64_t> hash_stupido_UNASeq(string sequenza, string spacedSeed);
 vector<vector<uint64_t>> hash_stupido_interaSeq(string spacedSeed);
-vector<uint64_t> metodoNuovo_UNASeq(string sequenza, string spacedSeed);
-vector<vector<uint64_t>> metodoNuovo_interaSeq(string spacedSeed);
+vector<uint64_t> metodoNuovo_UNASeq(string sequenza, string spacedSeedComplementato, vector<int> ab, vector<int> comandi);
+vector<vector<uint64_t>> metodoNuovo_interaSeq(string spacedSeedComplementato);
+vector<vector<uint64_t>> finalHash(vector<vector<uint64_t>> complete, vector<vector<uint64_t>> spaced);
 
 
 int main()
@@ -119,7 +120,7 @@ uint64_t primo_Hash(string sequenza_input, int spaceSeedSize) {
 uint64_t restanti_Hash(uint64_t precedente, char uscente, char entrante, int spaceSeedSize) {
 
 	//hash di quello che sta prima ruotato di 1 XOR hash carattere che esce ruotato di k XOR hash di quello che esce 
-	uint64_t hash = leftRotate(precedente, 1) ^ leftRotate(toInt(uscente), spaceSeedSize) ^ toInt(entrante);
+	uint64_t hash = ((leftRotate(precedente, 1)) ^ (leftRotate(toInt(uscente), spaceSeedSize))) ^ (toInt(entrante));
 	return hash;
 }
 
@@ -374,18 +375,56 @@ uint64_t metodoNuovo_restantiHash(string past, string token, vector<int> ab, uin
 	return 4;
 }
 
+//CONTROLLO FINO A QUA
+
 //calcola tutti gli hash per una intera sequenza e li mette dentro un vettore che poi ritorna  
-vector<uint64_t> metodoNuovo_UNASeq(string sequenza, string spacedSeed) {
+vector<uint64_t> metodoNuovo_UNASeq(string sequenza, string spacedSeedComplementato, vector<int> ab, vector<int> comandi) {
+
 	vector<uint64_t> hashSeq;
+	
+	int primi = (ab[0] - ab[1]);
+	//calcola i primi hash 
+	for (int i = 0; i <= primi; i++) {
+		string token;
+		for (int j = 0; j < spacedSeedComplementato.length(); j++) {
+			token += sequenza[i + j];
+		}
+		hashSeq.push_back(metodoNuovo_primiHash(token,spacedSeedComplementato));
+	}
+
+	//calcoliamo i restanti hash
+	//ogni possibile k sequenza dentro l'intera sequenza
+	for (int i = primi+1; i <= sequenza.length() - spacedSeedComplementato.length(); i++) {
+		//creo la stringa da dare in input al metodo che deve essere grande come lo spacedSeed
+		string token;
+		string past;
+		for (int j = 0; j < spacedSeedComplementato.length(); j++) {
+			token += sequenza[i + j];
+		}
+		for (int j = i-(primi); j < spacedSeedComplementato.length(); j++) {
+			past += sequenza[i-(primi) + j];
+		}
+		hashSeq.push_back(metodoNuovo_restantiHash(past,token,ab,hashSeq.at(i-(primi)),comandi));
+	}
+
+
 	return hashSeq; 
 }
 
 //calcola tutti gli hash di tutte le sequenze che ci sono dentro seq e che legge dal file
 vector<vector<uint64_t>> metodoNuovo_interaSeq(string spacedSeed) {
+
 	vector<vector<uint64_t>> fullHash;
+	//preprocessing 
+	string spacedSeedComplementato = complemento(spacedSeed);
+	vector<int> ab = preProcessing_1(spacedSeedComplementato);
+	vector<int> comandi = preProcessing_2(spacedSeedComplementato, ab);
+
+	for (int i = 0; i < seqs.size(); i++) {
+		fullHash.push_back(metodoNuovo_UNASeq(seqs.at(i), spacedSeedComplementato,ab,comandi));
+	}
 	return fullHash;
 }
-//CONTROLLO FINO A QUA /////////////
 
 
 //calcola l'hash in maniera "stupida" andando a vedere ogni 0 ed ogni 1 nello spaced seed e fa i conti di conseguenza
@@ -424,4 +463,19 @@ vector<vector<uint64_t>> hash_stupido_interaSeq(string spacedSeed) {
 		fullHash.push_back(hash_stupido_UNASeq(seqs.at(i),spacedSeed));
 	}
 	return fullHash;
+}
+
+//calcola la differenza tra l'hash completo e quello ottenuto con lo spaced seeds
+vector<vector<uint64_t>> finalHash(vector<vector<uint64_t>> complete, vector<vector<uint64_t>> spaced) {
+	vector<vector<uint64_t>> finalH;
+	vector<uint64_t> completeH;
+	vector<uint64_t> spacedH;
+	for (int i = 0; i < complete.size(); i++) {
+		completeH = complete.at(i);
+		spacedH = spaced.at(i);
+		for (int j = 0; j < complete.at(i).size(); j++) {
+			finalH.at(i).push_back(completeH.at(j) ^ spacedH.at(j));
+		}
+	}
+	return finalH;
 }
